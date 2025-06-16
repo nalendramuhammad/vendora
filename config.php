cf8f121b66aff8321920dccb72ca50afebbbf0fe
nalendra <94297761+nalendramuhammad@users.noreply.github.com>
2025-06-16 20:17:16 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Catalog');
+
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/');
+
+// DIR
+define('DIR_OPENCART', '/Applications/XAMPP/xamppfiles/htdocs/opencart/');
+define('DIR_APPLICATION', DIR_OPENCART . 'catalog/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_STORAGE', '/Applications/XAMPP/xamppfiles/htdocs/storage_opencart/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+define('DB_SSL_KEY', '');
+define('DB_SSL_CERT', '');
+define('DB_SSL_CA', '');
